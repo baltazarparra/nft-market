9d7ad18f93426c8ed353d3c19a49aa510b325b5f
baltazarparra <baltazarparra@outlook.com>
2023-02-09 20:25:24 -0300

    add tests

---
+++ src/backend/test/NFTMarketplace.test.js
@@ -0,0 +1,62 @@
+const { expect } = require("chai"); 
+
+const toWei = (num) => ethers.utils.parseEther(num.toString())
+const fromWei = (num) => ethers.utils.formatEther(num)
+
+describe("NFTMarketplace", function () {
+
+  let NFT;
+  let nft;
+  let Marketplace;
+  let marketplace
+  let deployer;
+  let addr1;
+  let addr2;
+  let addrs;
+  let feePercent = 1;
+  let URI = "sample URI"
+
+  beforeEach(async function () {
+    // Get the ContractFactories and Signers here.
+    NFT = await ethers.getContractFactory("NFT");
+    Marketplace = await ethers.getContractFactory("Marketplace");
+    [deployer, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+    // To deploy our contracts
+    nft = await NFT.deploy();
+    marketplace = await Marketplace.deploy(feePercent);
+  });
+
+  describe("Deployment", function () {
+
+    it("Should track name and symbol of the nft collection", async function () {
+      // This test expects the owner variable stored in the contract to be equal
+      // to our Signer's owner.
+      const nftName = "DApp NFT"
+      const nftSymbol = "DAPP"
+      expect(await nft.name()).to.equal(nftName);
+      expect(await nft.symbol()).to.equal(nftSymbol);
+    });
+
+    it("Should track feeAccount and feePercent of the marketplace", async function () {
+      expect(await marketplace.feeAccount()).to.equal(deployer.address);
+      expect(await marketplace.feePercent()).to.equal(feePercent);
+    });
+  });
+
+  describe("Minting NFTs", function () {
+
+    it("Should track each minted NFT", async function () {
+      // addr1 mints an nft
+      await nft.connect(addr1).mint(URI)
+      expect(await nft.tokenCount()).to.equal(1);
+      expect(await nft.balanceOf(addr1.address)).to.equal(1);
+      expect(await nft.tokenURI(1)).to.equal(URI);
+      // addr2 mints an nft
+      await nft.connect(addr2).mint(URI)
+      expect(await nft.tokenCount()).to.equal(2);
+      expect(await nft.balanceOf(addr2.address)).to.equal(1);
+      expect(await nft.tokenURI(2)).to.equal(URI);
+    });
+  })
+})
